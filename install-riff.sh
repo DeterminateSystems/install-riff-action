ce356bfd4535b85a8c70534b8cb07a292ff77313
Luc Perkins <lucperkins@gmail.com>
2022-09-06 23:09:35 +0300

    Enable supplying a Riff version

--- install-riff.sh
+++ install-riff.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 ARCH="x86_64"
-VERSION="1.0.0"
 SYSTEM=$(uname | tr '[:upper:]' '[:lower:]')
+# RIFF_VERSION is set by action.yml
 
-echo "::group::Installing riff ${VERSION} on ${SYSTEM}"
+echo "::group::Installing riff ${RIFF_VERSION} on ${SYSTEM}"
 
-INSTALL_URL="https://github.com/DeterminateSystems/riff/releases/download/v${VERSION}/riff-${ARCH}-${SYSTEM}"
+INSTALL_URL="https://github.com/DeterminateSystems/riff/releases/download/v${RIFF_VERSION}/riff-${ARCH}-${SYSTEM}"
 
 echo "::group::Downloading from ${INSTALL_URL}"
 
@@ -15,6 +15,6 @@ curl -Lo riff "${INSTALL_URL}"
 sudo mkdir -p /usr/local/bin
 sudo install -m +x ./riff /usr/local/bin/riff
 
-echo "::group::Riff ${VERSION} installed"
+echo "::group::Riff ${RIFF_VERSION} installed"
 
 exit 0
