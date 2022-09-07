40b1007eaf6c501a44f2373d3b6c940fa0e16446
Luc Perkins <lucperkins@gmail.com>
2022-09-07 10:15:34 +0300

    More docs and variablizing in shell script

--- install-riff.sh
+++ install-riff.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Support only standard ubuntu and macos runners for now
 ARCH="x86_64"
-SYSTEM=$(uname | tr '[:upper:]' '[:lower:]')
+
+# Other constants
+INSTALL_DIR="/usr/local/bin"
+EXECUTABLE="riff"
+
+# Variables
+SYSTEM=$(uname | tr '[:upper:]' '[:lower:]') # Ubuntu -> ubuntu or Darwin -> darwin
 # RIFF_VERSION is set by action.yml
-
-echo "::group::Installing riff ${RIFF_VERSION} on ${SYSTEM}"
-
 INSTALL_URL="https://github.com/DeterminateSystems/riff/releases/download/v${RIFF_VERSION}/riff-${ARCH}-${SYSTEM}"
 
+echo "::group::Installing riff ${RIFF_VERSION} on ${SYSTEM}"
 echo "::group::Downloading from ${INSTALL_URL}"
 
-curl -Lo riff "${INSTALL_URL}"
-sudo mkdir -p /usr/local/bin
-sudo install -m +x ./riff /usr/local/bin/riff
-rm riff
+# Installation process
+curl -Lo "${EXECUTABLE}" "${INSTALL_URL}"
+sudo mkdir -p "${INSTALL_DIR}"
+sudo install -m +x "${EXECUTABLE}" "${INSTALL_DIR}/${EXECUTABLE}"
+rm "${EXECUTABLE}"
 
 echo "::group::Riff ${RIFF_VERSION} installed"
 
